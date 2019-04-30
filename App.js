cc2397c40866042cfc85aa8ce9c14cc1481c74cb
scaulfield01 <s.caulfield88@gmail.com>
2019-04-30 12:11:09 -0700

    add axios package to replace fetch + implement dummy data using json server

--- App.js
+++ App.js
@@ -6,42 +6,20 @@ import {
   TextInput,
   FlatList
 } from 'react-native';
+import axios from 'axios';
+
 import Header from'components/Header';
 import RestaurantRow from 'components/RestaurantRow';
 
-// const restaurants = [
-//   {name: 'Component Bistro', address: '123 Real Street'},
-//   {name: 'Native Deli', address: '456 Generica Avenue'},
-//   {name: 'Facebook Gastro pub', address: '100 Markzis lane'},
-//   {name: 'Ratke Group', address: '00 Londonderry Terrace'},
-//   {name: 'Beatty and Sons', address: '209 Duke Alley'},
-//   {name: 'Becker Inc', address: '2247 Green Ridge Terrace'},
-//   {name: 'Jacobs Group', address: '72753 Waxwing Plaza'},
-//   {name: 'Hartmann, Wolf and Dietrich', address: '35 Westridge Plaza'},
-//   {name: 'Boyer-Corkery', address: '638 Continental Center'},
-//   {name: 'Littel, Goyette and Hansen', address: '2079 Cherokee Circle'},
-//   {name: 'Greenholt, Strosin and Volkman', address: '1560 Loftsgordon Crossing'},
-//   {name: 'Hamill Group', address: '321 Carpenter Pass'},
-//   {name: 'Ortiz, Goodwin and Anderson', address: '4 Tony Parkway'},
-//   {name: 'Bernier, Rath and Homenick', address: '36630 West Junction'},
-//   {name: 'Swaniawski Inc', address: '8847 Nelson Pass'},
-//   {name: 'Walker LLC', address: '283 Pankratz Pass'},
-//   {name: 'Prohaska-Howe', address: '3538 Hallows Avenue'},
-//   {name: 'Gulgowski-Bechtelar', address: '09537 Sunbrook Junction'},
-//   {name: 'Durgan-Koss', address: '9 Farwell Crossing'},
-//   {name: 'Ernser-Turner', address: '635 Spohn Pass'}
-// ];
-
-
 export default class App extends Component {
   state = {
-    search: null
+    search: null,
+    restaurants: []
   }
 
   componentDidMount(){
-    fetch('http://localhost:3000/restaurants')
-      .then(response => response.json())
-      .then(result => this.setState({ restaurants: result }))
+    axios.get('http://localhost:3000/restaurants')
+      .then(result => this.setState({ restaurants: result.data }))
   }
 
   render() {
